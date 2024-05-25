4653f9a64f5ce776221b371c7190b0299059365b
SANYAMCHAUDHARY22 <500083508@stu.upes.ac.in>
2024-05-26 01:51:18 +0530

    update3

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { View, Text, FlatList, StyleSheet,Button,Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+
 
 interface Destination {
   id: string;
@@ -8,15 +12,18 @@ interface Destination {
   bestTimeToTravel: string;
 }
 
+
+
 const destinations: Destination[] = [
-  { id: '1', name: 'Yamotri Temple', description: '', bestTimeToTravel: 'May to October', imageUri: 'https://www.bontravelindia.com/wp-content/uploads/2021/09/Yamunotri-Temple.jpg'},
-  { id: '2', name: 'Gangotri Temple', description: '' , bestTimeToTravel: 'May to October', imageUri: 'https://www.bontravelindia.com/wp-content/uploads/2021/09/Gangotri-Temple.jpg'},
-  { id: '3', name: 'Kedarnath Temple', description: '' , bestTimeToTravel: 'May to October', imageUri: 'https://st.adda247.com/https://currentaffairs.adda247.com/wp-content/uploads/multisite/sites/5/2023/06/16111548/Kedarnath.jpg'},
-  { id: '4', name: 'Badrinath Temple', description: '' , bestTimeToTravel: 'May to October', imageUri: 'https://www.bontravelindia.com/wp-content/uploads/2021/09/Badrinath-Temple.jpg'}
+  { id: '1', name: 'Yamotri Temple', description: 'Nestled in the Garhwal Himalayas, Yamunotri is revered as the source of the sacred Yamuna River, attracting pilgrims seeking spiritual solace amidst its serene surroundings.', bestTimeToTravel: 'May to October', imageUri: 'https://www.bontravelindia.com/wp-content/uploads/2021/09/Yamunotri-Temple.jpg'},
+  { id: '2', name: 'Gangotri Temple', description: 'Situated amidst the majestic peaks of the Himalayas, Gangotri holds profound significance as the origin of the holy Ganges River, drawing devotees and trekkers alike to its pristine landscapes.' , bestTimeToTravel: 'May to October', imageUri: 'https://www.bontravelindia.com/wp-content/uploads/2021/09/Gangotri-Temple.jpg'},
+  { id: '3', name: 'Kedarnath Temple', description: 'Nestled in the Garhwal Himalayas, Kedarnath is a sacred Hindu pilgrimage site renowned for its ancient temple dedicated to Lord Shiva, situated amidst stunning mountain scenery at an altitude of 3,583 meters.' , bestTimeToTravel: 'May to October', imageUri: 'https://st.adda247.com/https://currentaffairs.adda247.com/wp-content/uploads/multisite/sites/5/2023/06/16111548/Kedarnath.jpg'},
+  { id: '4', name: 'Badrinath Temple', description: 'Located in the Chamoli district of Uttarakhand, Badrinath is a revered pilgrimage destination housing the iconic Badrinath Temple dedicated to Lord Vishnu, attracting devotees seeking spiritual solace amidst the picturesque backdrop of the Himalayas.' , bestTimeToTravel: 'May to October', imageUri: 'https://www.bontravelindia.com/wp-content/uploads/2021/09/Badrinath-Temple.jpg'}
   // Add more destinations as needed
 ];
 
 const DestinationItem: React.FC<{ item: Destination }> = ({ item }) => (
+  
   <View style={styles.item}>
     <Image
     style={styles.image}
@@ -24,7 +31,9 @@ const DestinationItem: React.FC<{ item: Destination }> = ({ item }) => (
     />
     <Text style={styles.title}>{item.name}</Text>
     <Text>{item.description}</Text>
+    <Text></Text>
     <Text>Best Time For Travelling : {item.bestTimeToTravel}</Text>
+    
     <Button 
       title="View Details" 
       onPress={() => {
@@ -32,6 +41,7 @@ const DestinationItem: React.FC<{ item: Destination }> = ({ item }) => (
         console.log(`Navigating to ${item.name}`);
       }} 
     />
+   
   </View>
 );
 
@@ -49,9 +59,31 @@ export default function App() {
         renderItem={({ item }) => <DestinationItem item={item} />}
         keyExtractor={(item) => item.id}
       />
+      <View style={styles.footer}>
+  <Icon.Button 
+    name="facebook" 
+    backgroundColor="#3b5998"
+    onPress={this.loginWithFacebook}>
+    Facebook
+  </Icon.Button>
+  <Icon.Button 
+    name="twitter" 
+    backgroundColor="#1da1f2"
+    onPress={this.loginWithTwitter}>
+    Twitter
+  </Icon.Button>
+  <Icon.Button 
+    name="google" 
+    backgroundColor="#db4437"
+    onPress={this.loginWithGoogle}>
+    Google
+  </Icon.Button>
+   </View>
     </View>
+    
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -95,5 +127,15 @@ const styles = StyleSheet.create({
     height: 200,
     borderRadius: 10,
     marginBottom: 10,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around', // Align items evenly along the row
+    padding: 10,
+    backgroundColor: '#f8f8f8', // Light gray background
+    borderTopWidth: 10, // Add a border to the top of the footer
+    borderColor: '#e7e7e7', // Light gray border
+    alignItems: 'center', // Vertically center the icons
+    height: 65, // Set a fixed height for the footer
   },
 });
